98e34e07dac854cb76d715c2b3f23db53a68f8ca
PawelAcc1 <pawelbukowiec66@gmail.com>
2026-05-24 14:42:39 +0200

    modules instances connected on highest level of hierarchy

---
+++ projekt_basys3/fpga/rtl/top_ecg_basys3.sv
@@ -0,0 +1,85 @@
+/**
+ * San Jose State University
+ * EE178 Lab #4
+ * Author: prof. Eric Crabilla
+ *
+ * Modified by:
+ * 2025  AGH University of Science and Technology
+ * MTM UEC2
+ * Piotr Kaczmarczyk
+ *
+ * Description:
+ * Top level synthesizable module including the project top and all the FPGA-referred modules.
+ */
+
+module top_vga_basys3 (
+        input  wire clk,
+        input  wire btnC,
+        inout wire [2:1] JA,
+        output wire Vsync,
+        output wire Hsync,
+        output wire [3:0] vgaRed,
+        output wire [3:0] vgaGreen,
+        output wire [3:0] vgaBlue,
+        output wire JA1,
+    );
+
+    timeunit 1ns;
+    timeprecision 1ps;
+
+    /**
+     * Local variables for clock
+     */
+
+    wire locked;
+    wire clk_100MHz;
+    wire clk_65MHz;
+    wire pclk_mirror;
+    
+    /**
+     * Signals assignments
+     */
+
+    assign JA1 = pclk_mirror;
+
+    // Mirror pclk on a pin for use by the testbench;
+    // not functionally required for this design to work.
+
+    ODDR pclk_oddr (
+        .Q(pclk_mirror),
+        .C(clk_100MHz),
+        .CE(1'b1),
+        .D1(1'b1),
+        .D2(1'b0),
+        .R(1'b0),
+        .S(1'b0)
+    );
+
+    /*
+     * CLOCK IP CORE
+    */
+    clk_wiz_0 u_clk_wiz_0 (
+        .clk_in1(clk),
+        .locked(locked),
+        .clk100MHz(clk_100MHz),
+        .clk65MHz(clk_65MHz)
+    );
+
+    /**
+     *  Project functional top module
+     */
+
+    top_ecg u_top_ecg (
+        .clk_100MHz(clk_100MHz),
+        .clk_65MHz(clk_65MHz),
+        .rst_n(!btnC & locked),
+        .i2c_sda(JA[1]),
+        .i2c_scl(JA[2]),
+        .vs(Vsync),
+        .hs(Hsync),
+        .r(vgaRed),
+        .g(vgaGreen),
+        .b(vgaBlue)
+    );
+
+endmodule
--- projekt_basys3/rtl/top_ecg.sv
+++ projekt_basys3/rtl/top_ecg.sv
@@ -1,7 +1,10 @@
 // top_ecg.sv
 module top_ecg (
-        input  logic clk,
+        input  logic clk_100MHz,
+        input  logic clk_65MHz,
         input  logic rst_n,
+        inout wire i2c_sda,
+        output wire i2c_scl,
         output logic vs,
         output logic hs,
         output logic [3:0] r,
@@ -11,39 +14,87 @@ module top_ecg (
 
     timeunit 1ns;
     timeprecision 1ps;
+    /*
+     * INTER-MODULAR CONNECTIONS
+    */
+    wire start_sampling;
+    wire data_ready;
+    wire data_write_enable;
+    wire [15:0] filtered_data;
+    wire [9:0] read_address;
+    wire [11:0] ecg_data_read;
+    wire [15:0] ecg_data_received;
 
     vga_if if_tim();
-    vga_if if_bg();
+    vga_if if_grid();
+    vga_if if_render();
 
-    assign vs = if_rect.vsync;
-    assign hs = if_rect.hsync;
-    assign {r,g,b} = if_rect.rgb; //zmienic na rect na render
+    assign vs = if_render.vsync;
+    assign hs = if_render.hsync;
+    assign {r,g,b} = if_render.rgb; //zmienic na rect na render
 
+    /*
+     * INSTANCES OF MODULES
+    */
     vga_timing u_vga_timing (
-        .clk     (clk),
+        .clk     (clk_65MHz),
         .rst_n   (rst_n),
-        .vga_out (if_tim.out) 
+        .vga_out (if_tim) 
     );
 
     draw_grid u_draw_grid (
-        .clk      (clk),
+        .clk      (clk_65MHz),
         .rst_n    (rst_n),
-        .vga_in   (if_tim.in),
-        .vga_out  (if_bg.out)
+        .vga_in   (if_tim),
+        .vga_out  (if_grid)
     );
 
+    sampling_timer u_sampling_timer (
+        .clk            (clk_100MHz),
+        .rst_n          (rst_n),
+        .start_sampling (start_sampling)
+    );
 
+    i2c_master u_i2c_master (
+        .clk     (clk_100MHz),
+        .rst_n   (rst_n),
+        .start_sampling (start_sampling),
+        .SDA     (i2c_sda),
+        .SCL     (i2c_scl),
+        .adc_data(ecg_data_received),
+        .data_ready(data_ready)
+    );
 
-/*
- * FIR BANDPASS FILTER INSTANCE FROM IP CATALOG
-*/
-fir_compiler_0 u_fir_compiler_0 (
-  .aclk(aclk),                              // input wire aclk
-  .s_axis_data_tvalid(s_axis_data_tvalid),  // input wire s_axis_data_tvalid
-  .s_axis_data_tready(s_axis_data_tready),  // output wire s_axis_data_tready
-  .s_axis_data_tdata(s_axis_data_tdata),    // input wire [15 : 0] s_axis_data_tdata
-  .m_axis_data_tvalid(m_axis_data_tvalid),  // output wire m_axis_data_tvalid
-  .m_axis_data_tdata(m_axis_data_tdata)    // output wire [39 : 0] m_axis_data_tdata
-);
+    /*
+     * FIR BANDPASS FILTER INSTANCE FROM IP CATALOG
+    */
+    fir_compiler_0 u_fir_compiler_0 (
+    .aclk(clk_100MHz),                              // input wire clk
+    .s_axis_data_tvalid(data_ready),  // input wire s_axis_data_tvalid
+    .s_axis_data_tready(),  // output wire s_axis_data_tready
+    .s_axis_data_tdata(ecg_data_received),    // input wire [15 : 0] s_axis_data_tdata
+    .m_axis_data_tvalid(data_write_enable),  // output wire m_axis_data_tvalid
+    .m_axis_data_tdata(filtered_data)    // output wire [15 : 0] m_axis_data_tdata
+    );
+
+    ring_buffer u_ring_buffer (
+        .clk_read(clk_65MHz),
+        .clk_write(clk_100MHz),
+        .rst_n(rst_n),
+        .write_enable(data_write_enable),
+        .ecg_data_write(filtered_data[15:4]),
+        .read_address(read_address),
+        .ecg_data_read(ecg_data_read)
+    );
+
+    render_signal u_render_signal (
+        .clk(clk_65MHz),
+        .rst_n(rst_n),
+        .ecg_data_read(ecg_data_read),
+        .vga_in(if_grid),
+        .vga_out(if_render),
+        .read_address(read_address)
+    );
+
 
 endmodule
