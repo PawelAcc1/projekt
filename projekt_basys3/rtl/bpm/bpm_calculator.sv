a9193ac63d37d04c97460d1c8b071d9eea5777e9
PawelAcc1 <pawelbukowiec66@gmail.com>
2026-08-11 20:40:30 +0200

    added pipeline for calculating bpm to meet timing requirements

--- projekt_basys3/rtl/bpm/bpm_calculator.sv
+++ projekt_basys3/rtl/bpm/bpm_calculator.sv
@@ -13,13 +13,13 @@ module bpm_calculator (
     output logic bpm_instant_valid
 );
 
-    localparam logic [10:0] MAX_RR_SAMPLES = 11'd90000;
+    localparam logic [10:0] MAX_RR_SAMPLES = 11'd2000;
 
     logic [10:0] active_min_rr_samples;
     assign active_min_rr_samples = (min_rr_samples == 11'd0) ? 11'd150 : min_rr_samples;
 
     logic [10:0] sample_count;
-    logic [7:0]  bpm_history [0:9];
+    logic [7:0]  bpm_history [0:7];
     logic [3:0]  bpm_history_count;
     logic [11:0] bpm_history_sum;
     logic        timeout_fired;
@@ -29,6 +29,10 @@ module bpm_calculator (
     initial begin
         $readmemh("bpm_rom.hex", bpm_rom);
     end
+
+    // --- REJESTRY TYMCZASOWE (PIPELINE) ---
+    logic [10:0] captured_count;
+    logic        calc_stage_1;
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
@@ -41,7 +45,9 @@ module bpm_calculator (
             have_reference_peak <= 1'b0;
             bpm_history_count <= '0;
             bpm_history_sum   <= '0;
-            for (int i = 0; i < 10; i++)
+            captured_count    <= '0;
+            calc_stage_1      <= 1'b0;
+            for (int i = 0; i < 8; i++)
                 bpm_history[i] <= '0;
         end
         else begin
@@ -61,16 +67,13 @@ module bpm_calculator (
                     have_reference_peak <= 1'b0;
                     bpm_history_count <= '0;
                     bpm_history_sum   <= '0;
-                    for (int i = 0; i < 10; i++)
+                    calc_stage_1      <= 1'b0;
+                    for (int i = 0; i < 8; i++)
                         bpm_history[i] <= '0;
                 end
             end
 
             if (r_peak_detected) begin
-                automatic logic [7:0] current_bpm_raw;
-                automatic logic [11:0] next_history_sum;
-                automatic logic [3:0] next_history_count;
-
                 if (sample_count >= active_min_rr_samples && sample_count <= MAX_RR_SAMPLES) begin
                     timeout_fired <= 1'b0;
                     if (!have_reference_peak) begin
@@ -78,28 +81,10 @@ module bpm_calculator (
                         sample_count <= '0;
                     end
                     else begin
-                        current_bpm_raw = bpm_rom[sample_count];
-                        bpm_instant <= current_bpm_raw;
-                        bpm_instant_valid <= 1'b1;
-
-                        next_history_sum = bpm_history_sum + {4'b0, current_bpm_raw};
-                        next_history_count = (bpm_history_count < 4'd10)
-                                           ? (bpm_history_count + 4'd1)
-                                           : 4'd10;
-                        if (bpm_history_count == 4'd10)
-                            next_history_sum = next_history_sum - {4'b0, bpm_history[9]};
-
-                        for (int i = 9; i > 0; i--)
-                            bpm_history[i] <= bpm_history[i-1];
-                        bpm_history[0] <= current_bpm_raw;
-
-                        bpm_history_sum   <= next_history_sum;
-                        bpm_history_count <= next_history_count;
-                        bpm <= next_history_sum / next_history_count;
-
-                        bpm_valid <= 1'b1;
-
-                        sample_count <= '0;
+                        // STEP1: Latch data for following clock cycle
+                        captured_count <= sample_count;
+                        calc_stage_1   <= 1'b1;
+                        sample_count   <= '0;
                     end
                 end
                 else if (sample_count > MAX_RR_SAMPLES) begin
@@ -107,10 +92,43 @@ module bpm_calculator (
                     have_reference_peak <= 1'b1;
                     bpm_history_count <= '0;
                     bpm_history_sum   <= '0;
-                    for (int i = 0; i < 10; i++)
+                    calc_stage_1      <= 1'b0;
+                    for (int i = 0; i < 8; i++)
                         bpm_history[i] <= '0;
                     sample_count <= '0;
                 end
+            end
+            else if (calc_stage_1) begin
+                // STEP2: Calculate BPM
+                automatic logic [7:0] current_bpm_raw;
+                automatic logic [11:0] next_history_sum;
+                automatic logic [3:0] next_history_count;
+
+                current_bpm_raw = bpm_rom[captured_count];
+                bpm_instant <= current_bpm_raw;
+                bpm_instant_valid <= 1'b1;
+
+                next_history_sum = bpm_history_sum + {4'b0, current_bpm_raw};
+                next_history_count = (bpm_history_count < 4'd8)
+                                   ? (bpm_history_count + 4'd1)
+                                   : 4'd8;
+                                   
+                if (bpm_history_count == 4'd8)
+                    next_history_sum = next_history_sum - {4'b0, bpm_history[7]};
+
+                for (int i = 7; i > 0; i--)
+                    bpm_history[i] <= bpm_history[i-1];
+                bpm_history[0] <= current_bpm_raw;
+
+                bpm_history_sum   <= next_history_sum;
+                bpm_history_count <= next_history_count;
+                
+                bpm <= (next_history_count == 4'd8)
+                     ? ((next_history_sum + 12'd4) >> 3)
+                     : current_bpm_raw;
+
+                bpm_valid    <= 1'b1;
+                calc_stage_1 <= 1'b0; // Zero math stage flag
             end
         end
     end
